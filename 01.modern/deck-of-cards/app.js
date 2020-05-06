d6b4dfd4530e92f6abccbfe876798329b0e46e7c
Alex Reid <areid4118@gmail.com>
2020-05-06 10:04:44 -0400

    refactored deck of cards

--- 01.modern/deck-of-cards/app.js
+++ 01.modern/deck-of-cards/app.js
@@ -9,20 +9,19 @@
 // ---- clubs,spades, hearts, diamonds
 // Return both in an object
 
+function randomPick(arrayName) {
+	const randomArrayIndex = Math.floor(Math.random() * arrayName.length);
+	return arrayName[randomArrayIndex];
+}
+
 function getCard() {
 	const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
-	const randomValuesLength = Math.floor(Math.random() * values.length);
-	const randomValue = values[randomValuesLength];
-
 	const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
-	const randomSuitsLength = Math.floor(Math.random() * suits.length);
-	const randomSuit = suits[randomSuitsLength];
 
-	const randomCard = {
-		value: randomValue,
-		suit: randomSuit,
-	};
+	const randomValue = randomPick(values);
+	const randomSuit = randomPick(suits);
 
+	const randomCard = { value: randomValue, suit: randomSuit };
 	return randomCard;
 }
 
