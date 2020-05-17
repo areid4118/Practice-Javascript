c6eae55b4bc7017e8dce30a02d80566e8207f224
Alex Reid <areid4118@gmail.com>
2020-05-17 16:36:33 -0400

    fixed images that don't show up

--- 01.modern/projects/movie-finder/index.js
+++ 01.modern/projects/movie-finder/index.js
@@ -37,10 +37,11 @@ const onInput = async (event) => {
 	dropdown.classList.add('is-active');
 	for (const movie of movies) {
 		const option = document.createElement('a');
+		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
 		option.classList.add('dropdown-item');
 		option.innerHTML = `
-      <img src="${movie.Poster}" />
+      <img src="${imgSrc}" />
       ${movie.Title}
    `;
 
