7f7e8ecfbeb331b74ce52f4b423e2026c8f636f6
Alex Reid <areid4118@gmail.com>
2020-05-17 20:00:46 -0400

    added more html

--- 01.modern/projects/movie-finder/autocomplete.js
+++ 01.modern/projects/movie-finder/autocomplete.js
@@ -1,40 +1,38 @@
-/* eslint-disable no-loop-func */
 /* eslint-disable no-restricted-syntax */
-const createAutoComplete = ({ root, renderOption }) => {
+const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue, fetchData }) => {
 	root.innerHTML = `
-	<label><b>Search For a Movie</b></label>
-	<input class="input" />
-	<div class="dropdown">
-		<div class="dropdown-menu">
-			<div class="dropdown-content results"></div>
-		</div>
-	</div>
-`;
+   <label><b>Search</b></label>
+   <input class="input" />
+   <div class="dropdown">
+      <div class="dropdown-menu">
+         <div class="dropdown-content results"></div>
+      </div>
+   </div>
+   `;
 
 	const input = root.querySelector('input');
 	const dropdown = root.querySelector('.dropdown');
 	const resultsWrapper = root.querySelector('.results');
 
 	const onInput = async (event) => {
-		const movies = await fetchData(event.target.value);
+		const items = await fetchData(event.target.value);
 
-		if (!movies.length) {
+		if (!items.length) {
 			dropdown.classList.remove('is-active');
 			return;
 		}
 
 		resultsWrapper.innerHTML = '';
 		dropdown.classList.add('is-active');
-		for (const movie of movies) {
+		for (let item of items) {
 			const option = document.createElement('a');
 
 			option.classList.add('dropdown-item');
-			option.innerHTML = renderOption(movie);
-
+			option.innerHTML = renderOption(item);
 			option.addEventListener('click', () => {
 				dropdown.classList.remove('is-active');
-				input.value = movie.Title;
-				onMovieSelect(movie);
+				input.value = inputValue(item);
+				onOptionSelect(item);
 			});
 
 			resultsWrapper.appendChild(option);
