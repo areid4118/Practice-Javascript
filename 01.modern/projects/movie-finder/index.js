401d2089d94e3246c5c5e51ee1bbf88907864a2c
Alex Reid <areid4118@gmail.com>
2020-05-17 20:15:10 -0400

    add new input

--- 01.modern/projects/movie-finder/index.js
+++ 01.modern/projects/movie-finder/index.js
@@ -1,14 +1,10 @@
-createAutoComplete({
-	root: document.querySelector('.autocomplete'),
+const autoCompleteConfig = {
 	renderOption(movie) {
 		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 		return `
-			<img src="${imgSrc}" />
-			${movie.Title} (${movie.Year})
-		`;
-	},
-	onOptionSelect(movie) {
-		onMovieSelect(movie);
+		 <img src="${imgSrc}" />
+		 ${movie.Title} (${movie.Year})
+	  `;
 	},
 	inputValue(movie) {
 		return movie.Title;
@@ -27,9 +23,26 @@ createAutoComplete({
 
 		return response.data.Search;
 	},
+};
+
+createAutoComplete({
+	...autoCompleteConfig,
+	root: document.querySelector('#left-autocomplete'),
+	onOptionSelect(movie) {
+		document.querySelector('.tutorial').classList.add('is-hidden');
+		onMovieSelect(movie, document.querySelector('#left-summary'));
+	},
+});
+createAutoComplete({
+	...autoCompleteConfig,
+	root: document.querySelector('#right-autocomplete'),
+	onOptionSelect(movie) {
+		document.querySelector('.tutorial').classList.add('is-hidden');
+		onMovieSelect(movie, document.querySelector('#right-summary'));
+	},
 });
 
-const onMovieSelect = async (movie) => {
+const onMovieSelect = async (movie, summaryElement) => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params: {
 			apikey: 'd9835cc5',
@@ -37,45 +50,44 @@ const onMovieSelect = async (movie) => {
 		},
 	});
 
-	document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+	summaryElement.innerHTML = movieTemplate(response.data);
 };
 
 const movieTemplate = (movieDetail) => {
 	return `
-	<article class="media">
-		<figure class="media-left">
+	  <article class="media">
+		 <figure class="media-left">
 			<p class="image">
-				<img src="${movieDetail.Poster}" alt="" />
+			  <img src="${movieDetail.Poster}" />
 			</p>
-		</figure>
-		<div class="media-content">
+		 </figure>
+		 <div class="media-content">
 			<div class="content">
-				<h1>${movieDetail.Title}</h1>
-				<h4>${movieDetail.Genre}</h4>
-				<p>${movieDetail.Plot}</p>
+			  <h1>${movieDetail.Title}</h1>
+			  <h4>${movieDetail.Genre}</h4>
+			  <p>${movieDetail.Plot}</p>
 			</div>
-		</div>
-	</article>
-
-	<article class="notification is-primary">
-		<p class="title">${movieDetail.Awards}</p>
-		<p class="subtitle">Awards</p>
-	</article>
-	<article class="notification is-primary">
-		<p class="title">${movieDetail.BoxOffice}</p>
-		<p class="subtitle">Box Office</p>
-	</article>
-	<article class="notification is-primary">
-		<p class="title">${movieDetail.Metascore}</p>
-		<p class="subtitle">Metascore</p>
-	</article>
-	<article class="notification is-primary">
-		<p class="title">${movieDetail.imdbRating}</p>
-		<p class="subtitle">IMDB Rating</p>
-	</article>
-	<article class="notification is-primary">
-		<p class="title">${movieDetail.imdbVotes}</p>
-		<p class="subtitle">IMDB Votes</p>
-	</article>
+		 </div>
+	  </article>
+	  <article class="notification is-primary">
+		 <p class="title">${movieDetail.Awards}</p>
+		 <p class="subtitle">Awards</p>
+	  </article>
+	  <article class="notification is-primary">
+		 <p class="title">${movieDetail.BoxOffice}</p>
+		 <p class="subtitle">Box Office</p>
+	  </article>
+	  <article class="notification is-primary">
+		 <p class="title">${movieDetail.Metascore}</p>
+		 <p class="subtitle">Metascore</p>
+	  </article>
+	  <article class="notification is-primary">
+		 <p class="title">${movieDetail.imdbRating}</p>
+		 <p class="subtitle">IMDB Rating</p>
+	  </article>
+	  <article class="notification is-primary">
+		 <p class="title">${movieDetail.imdbVotes}</p>
+		 <p class="subtitle">IMDB Votes</p>
+	  </article>
 	`;
 };
