9cc8278fffce77f272f3a6b052c856078b7498ac
Alex Reid <areid4118@gmail.com>
2020-05-27 16:36:34 -0400

    able to all books to the list. good checkpoint

--- 06.youtube-projects/travesy-media/booklist/app.js
+++ 06.youtube-projects/travesy-media/booklist/app.js
@@ -41,6 +41,12 @@ class UI {
 
 		list.appendChild(row);
 	}
+
+	static clearFields() {
+		document.querySelector('#title').value = '';
+		document.querySelector('#author').value = '';
+		document.querySelector('#isbn').value = '';
+	}
 }
 
 // Store Class: Handles Storage
@@ -49,5 +55,21 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (event) => {
+	event.preventDefault();
 
+	// Get form values
+	const title = document.querySelector('#title').value;
+	const author = document.querySelector('#author').value;
+	const isbn = document.querySelector('#isbn').value;
+
+	// Instantiate book
+	const book = new Book(title, author, isbn);
+
+	// Add Book to UI
+	UI.addBookToList(book);
+
+	// Clear fields
+	UI.clearFields();
+});
 // Event: Remove a Book
